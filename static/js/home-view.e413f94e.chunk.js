a316f68fcac6edc59a4f6aee39cca9d41a2d9a10
Vitalii-Mamchur <vetalpatsi@gmail.com>
2021-08-08 03:01:51 +0300

    Updates

---
+++ static/js/home-view.e413f94e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[1],{131:function(e,o,t){},136:function(e,o,t){"use strict";t.r(o);t(0),t(131);var n=t(2);o.default=function(){return Object(n.jsx)("div",{className:"HomeContainer",children:Object(n.jsx)("h1",{className:"Home-title",children:"Your Phonebook"})})}}}]);
+//# sourceMappingURL=home-view.e413f94e.chunk.js.map
